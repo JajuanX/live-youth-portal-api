8913f21ac2e362628a8e9a0bd75c352dbfc6abad
jajuanx <jajuan.burton@live.com>
2025-06-21 20:52:24 -0400

    #2 - update middleware to protect routes

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,21 +1,21 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
 
-const initialize = (req, res, next) => {
+const protect = async (req, res, next) => {
 	const authHeader = req.headers.authorization;
 
-	if (!authHeader || !authHeader.startsWith('Bearer ')) {
-		return res.status(401).json({ message: 'Unauthorized: No token provided' });
+	if (!authHeader?.startsWith('Bearer ')) {
+		return res.status(401).json({ message: 'No token provided' });
 	}
 
-	const token = authHeader.split(' ')[1];
-
 	try {
+		const token = authHeader.split(' ')[1];
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-		req.user = decoded;
+		req.user = await User.findById(decoded.id).select('-password');
 		next();
 	} catch (err) {
-		res.status(401).json({ message: 'Unauthorized: Invalid token' });
+		return res.status(401).json({ message: 'Invalid token' });
 	}
 };
 
-export default { initialize };
+export default { protect };
